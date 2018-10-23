2f89b319c9908932d0a9c980b6457b90b66525cf
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 21:49:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,6 +31,7 @@ attr_accessor :name, :songs
   def self.find_or_create_by_name(name)
     artist = self.find_by_name(name)
     artist = self.create_by_name(name) if !artist
+    artist
   end
 
   def print_songs
