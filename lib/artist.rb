9602bf17deb9caca41e8684ccf93820bafbdfd0c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 21:12:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,12 @@
 class Artist
-attr_accessor :name
+attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def add_songs(song)
+    @songs << song
   end
 end
