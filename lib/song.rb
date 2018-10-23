ca1ba68574a49ff42a3e252be6c362390009183a
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 21:36:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,7 @@ class Song
 
   def self.new_by_filename(filename)
     data = filename.sub(".mp3", "").split(" - ")
+    binding.pry
     new_song = Song.new(data[1])
     new_song.artist = Artist.find_or_create_by_name(data[0])
     new_song.genre = data[2]
