7a716170de5ec4d96e03f3e056da56634b726888
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 21:45:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,14 @@ class Song
 
   def self.new_by_filename(filename)
     data = filename.sub(".mp3", "").split(" - ")
-    binding.pry
     new_song = Song.new(data[1])
-    new_song.artist = Artist.find_or_create_by_name(data[0])
+    self.attach_song_to_artist(new_song, data[0])
     new_song.genre = data[2]
     new_song
+  end
+
+  def self.attach_song_to_artist(song, artist)
+    song.artist = Artist.find_or_create_by_name(data[0])
+    song.artist.add_song(song)
   end
 end
