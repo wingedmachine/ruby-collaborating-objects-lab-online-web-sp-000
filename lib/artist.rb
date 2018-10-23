78187a22dd7a1363376e36f1d3e95deaea69ee47
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 21:19:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,7 @@ attr_accessor :name, :songs
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
@@ -23,6 +24,10 @@ attr_accessor :name, :songs
     @@all.detect { |artist| artist.name == name}
   end
 
+  def self.create_by_name(name)
+    Artist.new(name)
+  end
+  
   def self.find_or_create_by_name(name)
     self.create_by_name(name) if !self.find_by_name(name)
   end
